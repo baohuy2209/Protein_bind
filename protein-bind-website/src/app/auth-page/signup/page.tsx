684a7feca853e56c387cfd93baa598b03dbfbde6
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-09-03 23:45:40 +0700

    sign-up page

--- protein-bind-website/src/app/auth-page/signup/page.tsx
+++ protein-bind-website/src/app/auth-page/signup/page.tsx
@@ -1,5 +1,44 @@
+"use client";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import ComponentHeader from "@/components/ComponentHeader/ComponentHeader";
+import { createUser } from "@/lib/actions/user.actions";
+import {
+  CameraIcon,
+  LoaderCircle,
+  LockIcon,
+  MailIcon,
+  UserIcon,
+} from "lucide-react";
 const Signup: React.FC = () => {
-  return <div>Signup</div>;
+  const [user, setUser] = React.useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    confirmPassword: "",
+    photo: "",
+    userBio: "",
+  });
+  const [imageFile, setImageFile] = React.useState<File | null>(null);
+  const [errors, setErrors] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handeFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <div>
+      <ComponentHeader pageName="Sign Up" />
+    </div>
+  );
 };
 export default Signup;
